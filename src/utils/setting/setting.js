4dd66c825c117229d15d3cb7fd6de13f7d8771fd
Hieund <ngoduyhieu66@gmail.com>
2022-05-12 16:59:41 +0700

    create project

---
+++ src/utils/setting/setting.js
@@ -0,0 +1,11 @@
+
+export const TOKEN_FIVERR = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCDEkMOgIE7hurVuZyAwNSIsIkhldEhhblN0cmluZyI6IjE1LzA5LzIwMjIiLCJIZXRIYW5UaW1lIjoiMTY2MzIwMDAwMDAwMCIsIm5iZiI6MTYzNDgzNTYwMCwiZXhwIjoxNjYzMzQ3NjAwfQ.uVU26Zzhj9Tt11v92mEFOSGk1Ow-on5dWy9q9vuSVt4'
+export const DOMAIN = 'fiverr.cybersoft.edu.vn'
+
+export const GROUP_ID = 'GP04'
+
+export const TOKEN = 'accessToken'
+
+
+
+
---
+++ src/utils/setting/settingAxios.js
@@ -0,0 +1,18 @@
+import axios from "axios"
+import { DOMAIN, TOKEN, TOKEN_FIVERR } from "./setting"
+
+
+export const http = axios.create({
+    baseURL:DOMAIN,
+    timeout:30000
+})
+http.interceptors.request.use((config) => { 
+    config.headers = {
+        ...config.headers,
+        'TokenCybersoft': TOKEN_FIVERR ,
+        'Authorization': 'Bearer ' + JSON.parse(localStorage.getItem(TOKEN))
+    }
+    return config
+ }, (errors)=>{
+     return Promise.reject(errors)
+ })
