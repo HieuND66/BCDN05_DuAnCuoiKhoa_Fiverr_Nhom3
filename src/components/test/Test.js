d31860dd5a60361a0f29de212bea1386ebabdd0d
Hieund <ngoduyhieu66@gmail.com>
2022-05-13 11:23:32 +0700

    setup variable

---
+++ src/components/test/Test.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './test.scss'
+import '../../scss/_button.scss'
+export default function Test() {
+  return (
+    <div className='test'>
+      <button className='btn btn-green mr-4'>Test</button>
+      <button className='btn btn-green-input mr-4'>Test</button>
+      <button className='btn btn-white'>Test</button>
+    </div>
+  )
+}
