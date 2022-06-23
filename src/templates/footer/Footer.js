2280b68c56b8e7885bdfccb8c71f7fbd88db3dd5
Hieund <ngoduyhieu66@gmail.com>
2022-06-24 01:12:05 +0700

    update

--- src/templates/footer/Footer.js
+++ src/templates/footer/Footer.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import '../../pages/Home/Home.scss'
 export default function Footer() {
+     
   return (
     <footer>
                     <div className="container mt-20" >
