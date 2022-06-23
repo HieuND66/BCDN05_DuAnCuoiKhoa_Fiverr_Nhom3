a580f42ca430cb1ef1bce7e124006b1267003213
Hieund <ngoduyhieu66@gmail.com>
2022-06-23 13:20:30 +0700

    change domain http --> https

--- src/utils/setting/setting.js
+++ src/utils/setting/setting.js
@@ -1,6 +1,6 @@
 
 export const TOKEN_FIVERR = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCDEkMOgIE7hurVuZyAwNSIsIkhldEhhblN0cmluZyI6IjE1LzEwLzIwMjIiLCJIZXRIYW5UaW1lIjoiMTY2NTc5MjAwMDAwMCIsIm5iZiI6MTYzNDgzNTYwMCwiZXhwIjoxNjY1OTM5NjAwfQ.JnZ2TCDw6qCOkgVFEoDB-LbIivUOb5JgXbCraWktEKQ'
-export const DOMAIN = 'http://fiverr.cybersoft.edu.vn'
+export const DOMAIN = 'https://fiverr.cybersoft.edu.vn'
 
 export const GROUP_ID = 'GP04'
 
